1b53f0f7570ae728a6e882d584250aea6e95e339
sean <277578635@qq.com>
2018-01-21 12:09:32 +0800

    该网站的中心控制器

--- src/main/java/com/safewind/sean/controller/CenterController.java
+++ src/main/java/com/safewind/sean/controller/CenterController.java
@@ -1,39 +1,33 @@
 package com.safewind.sean.controller;
 
-import com.alibaba.druid.util.StringUtils;
 import com.safewind.sean.po.User;
 import com.safewind.sean.service.UserService;
 import com.safewind.sean.util.ValidateCode;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.subject.Subject;
-import org.apache.shiro.web.util.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 @Controller
+
 public class CenterController {
     @Autowired
     UserService userService;
     @Autowired
     User user;
-    @RequestMapping("login")
+    @RequestMapping("/login")
     public String login()
     {
         return "login";
 
     }
-
 
     /**
      * 生成验证码
@@ -44,23 +38,38 @@ public class CenterController {
     @RequestMapping(value = "/validateCode/imageProduce")
     public void validateCode(HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setHeader("Cache-Control", "no-cache");
-        String verifyCode = ValidateCode.generateTextCode(ValidateCode.TYPE_NUM_ONLY, 4, null);
+        String verifyCode = ValidateCode.generateTextCode(ValidateCode.TYPE_ALL_MIXED, 4, null);
         request.getSession().setAttribute("validateCode", verifyCode);
         response.setContentType("image/jpeg");
         BufferedImage bim = ValidateCode.generateImageCode(verifyCode, 90, 30, 3, true, Color.WHITE, Color.BLACK, null);
         ImageIO.write(bim, "JPEG", response.getOutputStream());
     }
-    @RequestMapping("index")
+    @RequestMapping("/index")
     public String Index()
     {
         return "index";
     }
 
-    @RequestMapping("main")
+    @RequestMapping("/main")
     public String Main()
     {
         return "main";
     }
+
+    @RequestMapping("/home")
+    public String Home()
+    {
+        return "home";
+    }
+
+
+    @RequestMapping("/search")
+    public String Search()
+    {
+
+        return "/search/search";
+    }
+
 
 
 }
